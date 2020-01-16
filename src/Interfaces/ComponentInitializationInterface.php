f3defedbf355839de9bf7b7769d8655776fc71ed
Vesko Kenashkov <vesko@azonmedia.com>
2020-01-16 13:25:35 +0200

    wip

---
+++ src/Interfaces/ComponentInitializationInterface.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace GuzabaPlatform\Components\Base\Interfaces;
+
+use Guzaba2\Kernel\Interfaces\ClassInitializationInterface;
+
+interface ComponentInitializationInterface extends ClassInitializationInterface
+{
+
+}
